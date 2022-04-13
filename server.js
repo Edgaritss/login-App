5863899b01ac719ce022729fbb8583920e7a6ad2
Edgar Rojas <garos19480@gmail.com>
2022-04-13 16:45:33 -0500

    heroku 2

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/app-login'));
+app.use(express.static('./dist/login-App'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app-login/'}),
+    res.sendFile('index.html', {root: 'dist/login-App/'}),
 );
 
 // Start the app by listening on the default Heroku port
