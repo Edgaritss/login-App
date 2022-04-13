fcc51c977f1c0586b11f96475e00d35b0d11f8b3
Edgar Rojas <garos19480@gmail.com>
2022-04-13 15:13:52 -0500

    heroku 3

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app=express();
-app.use(express.static(__dirname+ '/login-app'));
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname+
-        '/login-app/login.html'
-        ));});
+
+app.use(express.static('./dist/login-app'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('login.html', {root: 'dist/login-app/'}),
+);
 
 app.listen(process.env.PORT ||8080); 
