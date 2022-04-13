f3cc50b5e200fc5850bf3af45bc03cf1144506a7
Edgar Rojas <garos19480@gmail.com>
2022-04-13 15:36:23 -0500

    heroku 3

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + './dist/login-app'));
+
+app.use(express.static('./dist/login-app'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/login-app/'}),
